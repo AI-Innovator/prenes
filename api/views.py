7d6559ae61e8a942abb7fc3a9ed2ec373c78376b
Akash02 <snow_man22@outlook.com>
2021-08-27 12:57:02 -0400

    first commit

---
+++ api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, status
+from .serializers import TodoSerializer
+
+from .models import Todo
+# Create your views here.
+
+
+class TodoListView(generics.ListAPIView):
+    model = Todo
+    serializer_class = TodoSerializer
